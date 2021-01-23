283c895c1d550ff225a1485ecb119a7f5e07e65e
Dani <Dani@EIL8R3PCCE.local>
2021-01-23 18:54:32 +0100

    Late init, emacs config files

---
+++ .emacs.d/init.el
@@ -0,0 +1,32 @@
+
+(setq mac-right-command-modifier 'none
+      mac-right-option-modifier 'none)
+
+(setq inhibit-startup-screen t)
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(load-theme 'tsdh-dark)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
